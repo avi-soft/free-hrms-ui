bf5e104778ab513b55b7f9b141f6dc86f987cd0e
kirandeep7889 <124796483+kirandeep7889@users.noreply.github.com>
2024-07-02 13:12:06 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
+
+
+ReactDOM.createRoot(document.getElementById('Root')).render(
+        <Provider store={store}>
+          <BrowserRouter>
+              <App />
+              <Toaster/>
+          </BrowserRouter>
+        </Provider>
+
+)
